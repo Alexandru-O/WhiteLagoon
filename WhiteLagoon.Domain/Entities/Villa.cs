82e1629e3004cf88ca886168f41ad984ed93511b
Alexandru Ologu <ologu.alexandru@gmail.com>
2024-01-23 15:57:50 +0200

    Villa Availability

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -34,5 +34,8 @@ namespace WhiteLagoon.Domain.Entities
 
         [ValidateNever]
         public IEnumerable<Amenity> VillaAmenity { get; set; }
+
+        [NotMapped]
+        public bool IsAvailable { get; set; } = true;
     }
 }
